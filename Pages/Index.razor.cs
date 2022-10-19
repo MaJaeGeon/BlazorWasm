ee2d6b1bf5c3cf62f9c75a6ea97873b1583b7793
MaJaeGeon <akworjs0517@gmail.com>
2022-10-19 14:44:51 +0000

    update

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,72 +1,65 @@
 using GraphQL;
 using GraphQL.Client.Http;
-using Microsoft.AspNetCore.Components;
 using GraphQL.Client.Serializer.SystemTextJson;
+using Microsoft.AspNetCore.Components;
 
-namespace BlazorWasm.Pages
-{
-    public partial class Index : ComponentBase{
-    protected List<Entry>? Items;
+namespace  BlazorWasm.Pages {
+    public partial class Index : ComponentBase {
+        [Parameter]
+        public string? link { get; set; }
+        protected List<Entry>? Items;
 
-        protected override async Task OnInitializedAsync()
+        protected override async Task OnParametersSetAsync()
         {
-            GraphQLHttpClient graphQLClient = new GraphQLHttpClient("https://api.example.com/graphql", new SystemTextJsonSerializer());
-
-             var personAndFilmsRequest = new GraphQLRequest {
-                Query =@"
-                    query GetPosts($repoName: String!, $repoOwner: String!, $path: String){
-                        repository(name: $repoName, owner: $repoOwner) {
-                                object(expression: $path) {
-                                    ... on Tree {
-                                    entries {
-                                        name
-                                        path
-                                    }
+            GraphQLHttpClient graphQLClient = new GraphQLHttpClient("https://api.github.com/graphql", new SystemTextJsonSerializer());
+                
+            var personAndFilmsRequest = new GraphQLRequest {
+            Query =@"
+                query GetPosts($repoName: String!, $repoOwner: String!, $path: String){
+                    repository(name: $repoName, owner: $repoOwner) {
+                            object(expression: $path) {
+                                ... on Tree {
+                                entries {
+                                    name
+                                    path
                                 }
                             }
                         }
                     }
-                }",
-                Variables = new {
-                    repoName = "majaegeon.github.io",
-                    repoOwner = "MaJaeGeon",
-                    path = $"HEAD:posts/{link}"
                 }
-            };
-
-
+            ",
+            Variables = new {
+                repoName = "majaegeon.github.io",
+                repoOwner = "MaJaeGeon",
+                path = $"HEAD:posts/{link}"
+            }};
+        
+            graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", $"bearer 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");
             var graphqlResponse = await graphQLClient.SendQueryAsync<Posts>(personAndFilmsRequest);
-            Items = graphqlResponse.Data.Data.Repository.Object.Entries;
+
+            Items = graphqlResponse.Data.Repository.Object.Entries;
         }
 
-        [Parameter]
-        public string? link { get; set; }
-    }
 
-    public class Data
-    {
-        public Repository Repository { get; set; }
-    }
+        public class Posts
+        {
+            public Repository Repository { get; set; }
+        }
 
-    public class Entry
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-    }
+        public class Entry
+        {
+            public string Name { get; set; }
+            public string Path { get; set; }
+        }
 
-    public class Object
-    {
-        public List<Entry> Entries { get; set; }
-    }
+        public class Object
+        {
+            public List<Entry> Entries { get; set; }
+        }
 
-    public class Repository
-    {
-        public Object Object { get; set; }
+        public class Repository
+        {
+            public Object Object { get; set; }
+        }
     }
-
-    public class Posts
-    {
-        public Data Data { get; set; }
-    }
-
 }
