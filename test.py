8a12e0bb49edd043e251b9770df0ad1b40a3416c
MaJaeGeon <akworjs0517@gmail.com>
2022-11-02 12:45:59 +0000

    1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import json
 
 def path_to_dict(path):
-    d = {'name': os.path.basename(path), 'path': os.path.abspath(path)}
+    d = {'name': os.path.basename(path), 'path': os.path.relpath(path)}
     if os.path.isdir(path):
         d['type'] = "directory"
         d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
